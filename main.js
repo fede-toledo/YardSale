505c6d3d45e5f2c5c6c2a4326e63787b279ceef5
Fede Toledo <fede.toledo5@gmail.com>
2024-06-17 20:35:06 -0300

    Agregamos el DesktopMenu a nuestro header

---
+++ main.js
@@ -0,0 +1,8 @@
+const navEmail = document.querySelector('.navbar-email');
+const dekstopMenu = document.querySelector('.desktop-menu');
+
+navEmail.addEventListener('click', toggleDesktopMenu);
+
+function toggleDesktopMenu () {
+    dekstopMenu.classList.toggle('inactive');
+}
