be01225e1d7027e394e7fda1339dd6a405fa58c8
Fede Toledo <fede.toledo5@gmail.com>
2024-06-17 21:08:09 -0300

    Se agregó el menu mobile y las medias queries para mostrarlo/ocultarlo. Además se optimizó el código css en esas medias queries.

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const navEmail = document.querySelector('.navbar-email');
 const dekstopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 navEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu () {
     dekstopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu () {
+    mobileMenu.classList.toggle('inactive');
 }
